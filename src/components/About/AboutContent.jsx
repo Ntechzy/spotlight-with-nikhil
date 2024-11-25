2a5f8ac8bc3ecb7a53da42e915189da0d0d8acf8
pankaj-Ntechzy <pankaj@ntechzy.in>
2024-11-25 17:53:48 +0530

    about page responsive issue solved:

--- src/components/About/AboutContent.jsx
+++ src/components/About/AboutContent.jsx
@@ -3,7 +3,7 @@ const AboutContent = () => {
   return (
     <div className="flex items-center justify-between p-6 bg-gray-100" data-aos="fade-up">
       {/* Content Section */}
-      <div className="w-1/2 p-4">
+      <div className="w-full md:w-1/2 p-4">
         <h1 className="text-2xl font-bold mb-4">Welcome to Spotlight with Nikhil</h1>
         <p>
           We bridge the gap between the public and decision-makers, bringing you insights and answers
@@ -12,11 +12,11 @@ const AboutContent = () => {
         </p>
       </div>
       {/* Image Section */}
-      <div className="w-1/2 p-4">
-        <img 
-          src="/assets/podcastbanner.JPG" 
-          alt="Spotlight" 
-          className="w-full h-auto rounded-lg" 
+      <div className="w-full md:w-1/2 p-4">
+        <img
+          src="/assets/podcastbanner.JPG"
+          alt="Spotlight"
+          className="w-full h-auto rounded-lg"
         />
       </div>
     </div>
